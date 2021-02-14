b0292f4294a42122fe7993d2e99e7bdecfe2014d
alex hawley <alex@engage2learn.org>
2021-02-14 17:56:43 -0600

    - First Commit

--- Quizzler-iOS13/Question.swift
+++ Quizzler-iOS13/Question.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+struct Question {
+    let question: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        self.question = q
+        self.answer = a
+    }
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -5,16 +5,79 @@
 //  Created by Angela Yu on 12/07/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var questionProgress: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var questionNumber = 1
+    var questionIndex = 0
+    var quiz: [Question] = [
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+    ]
+    var sentAnswer = ""
+    var totalAnswers = 0;
+    var rightAnswers = 0;
+    var wrongAnswers = 0;
+    var answers = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = quiz[questionIndex].question
+        totalAnswers = quiz.count;
+        questionProgress.progress = 0.0
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        sentAnswer = sender.currentTitle!
+        if (sentAnswer != "") {
+            if (questionIndex < quiz.count) {
+                if (sentAnswer == quiz[questionIndex].answer) {
+                    rightAnswers += 1
+                    // print("Correct")
+                } else {
+                    wrongAnswers += 1
+                    // print("Wrong")
+                }
+                answers += 1
+              /*/
+                print("Correct Answers: ", rightAnswers, " / ", totalAnswers)
+                print("Wrong Answers: ", wrongAnswers, " / ", totalAnswers)
+                print("Answers: ", answers, " / ", totalAnswers)
+                // */
+            }
+            
+            if (questionIndex < quiz.count - 1) {
+                let anwsersF = Float(answers)
+                let totalAnswersF = Float(totalAnswers)
+                questionProgress.progress = anwsersF / totalAnswersF
+                questionIndex += 1
+                questionNumber += 1
+                updateUI()
+            } else if (questionIndex < quiz.count) {
+                questionIndex += 1
+                questionNumber += 1
+                questionProgress.progress = 1;
+            }
+        }
+    }
+    func updateUI() {
+        questionLabel.text = quiz[questionIndex].question
+    }
 }
-
